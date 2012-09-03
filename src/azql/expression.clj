d869deee440cf38b66e3d900117024ef1eef44af
Andrei Zhlobich <a.zhlobich@gmail.com>
2012-09-03 23:47:45 +0300

    Render functions using multimethod

--- src/azql/expression.clj
+++ src/azql/expression.clj
@@ -34,78 +34,12 @@
       (replace "%" (str like-pattern-escaping-char "%"))
       (replace "_" (str like-pattern-escaping-char "_"))))
 
-(def default-expression-rendering-fns
-  {'raw (fn [s] (raw s))
-   'and (fn [x & r] (par (interpose AND (cons x r))))
-   'or  (fn [x & r] (par (interpose OR (cons x r))))
-   '= (fn [a b]
-        (par
-         (cond
-          (nil? a) [b IS_NULL]
-          (nil? b) [a IS_NULL]
-          :else [a EQUALS b])))
-   '<> (fn [a b]
-         (par
-          (cond
-           (nil? a) [b IS_NOT_NULL]
-           (nil? b) [a IS_NOT_NULL]
-           :else [a NOT_EQUALS b])))
-   '+ (fn
-        ([x] (par [UPLUS x]))
-        ([x & r] (par (interpose PLUS (cons x r)))))
-   '- (fn
-        ([x] (par [UMINUS x]))
-        ([a & r] (par (interpose MINUS (cons a r)))))
-   '* (fn
-        [x & r] (par (interpose MULTIPLY (cons x r))))
-   '/ (fn
-        [x y] (par [x DIVIDE y]))
-   'not (fn [x](par NOT x))
-   '< (fn [a b] (par a LESS b))
-   '> (fn [a b] (par a GREATER b))
-   '<= (fn [a b] (par a LESS_EQUAL b))
-   '>= (fn [a b] (par a GREATER_EQUAL b))
-   'nil? (fn [x] (par x IS_NULL))
-   'not-nil? (fn [x] (par x IS_NOT_NULL))
-   'not-in? (fn [a b]
-              (cond
-               (empty? b) const-true
-               (map? b) (par a NOT_IN (parenthesis b))
-               :else (par a NOT_IN (parenthesis (comma-list b)))))
-   'in? (fn [a b]
-          (cond
-           (empty? b) const-false
-           (map? b) (par a IN (parenthesis b))
-           :else (par a IN (parenthesis (comma-list b)))))
-   'count (fn
-            ([r] [COUNT (parenthesis r)])
-            ([d r]
-               (case d
-                :distinct [COUNT (par DISTINCT r)]
-                nil [COUNT (par r)]
-                (illegal-argument "Unknown modifier " d))))
-   'max (fn [x] [MAX (par x)])
-   'min (fn [x] [MIN (par x)])
-   'avg (fn [x] [AVG (par x)])
-   'sum (fn [x] [SUM (par x)])
-   'exists? (fn [q] (par EXISTS (par q)))
-   'not-exists? (fn [q] (par NOT_EXISTS (par q)))
-   'some (fn
-            ([q] [SOME (par q)])
-            ([f q] [SOME (par (attach-field f q))]))
-   'any (fn
-            ([q] [ANY (par q)])
-            ([f q] [ANY (par (attach-field f q))]))
-   'all (fn
-            ([q] [ALL (par q)])
-            ([f q] [ALL (par (attach-field f q))]))
-   'like? (fn [a b]
-            [a LIKE b ESCAPE like-pattern-escaping-sql])
-   'begins? (fn [a b]
-                  (check-argument (string? b) "Pattern should be string.")
-                  [a LIKE (str (escape-like-pattern b) "%")
-                   ESCAPE like-pattern-escaping-sql])
-   })
+(defmulti render-fn (fn [f & _] f))
+
+(defn- operator?
+  [f]
+  (let [fch (char (get (name f) 0))]
+    (not (Character/isLetterOrDigit fch))))
 
 (defn render-generic-operator
   "Render generic infix operator"
@@ -122,26 +56,17 @@
      (comma-list r)
      (raw ")")]))
 
-(defn- operator?
-  [f]
-  (let [fch (char (get (name f) 0))]
-    (not (Character/isLetterOrDigit fch))))
+(defmethod render-fn :default
+  [s & r]
+  (if (operator? s)
+    (apply render-generic-operator s r)
+    (apply render-generic-function s r)))
 
 (defn- canonize-operator-symbol
   [s]
   (when-not (symbol? s)
     (illegal-argument "Illegal function name '" s "', extected symbol"))
   (expression-synonym s s))
-
-(defn render-operator-or-function
-  "Render operator/function, recognize synonyms."
-  [f r]
-  (let [s (expression-synonym f f)]
-    (if-let [c (default-expression-rendering-fns s)]
-      (apply c r)
-      (if (operator? s)
-        (apply render-generic-operator f r)
-        (apply render-generic-function f r)))))
 
 (defn prepare-macro-expression
   "Walk tree and replace synonyms.
@@ -172,5 +97,146 @@
   (if (and (sequential? etree) (symbol? (first etree)))
     (let [[f & r] etree
           rs (map render-expression r)]
-      (render-operator-or-function f rs))
+      (apply render-fn f rs))
     etree))
+
+
+;; operators
+
+(defmethod render-fn 'raw
+  [_ s]
+  (raw s))
+
+(defmethod render-fn 'and
+  [_ x & r]
+  (par (interpose AND (cons x r))))
+
+(defmethod render-fn 'or
+  [_ x & r]
+  (par (interpose OR (cons x r))))
+
+(defmethod render-fn '=
+  [_ a b]
+  (par
+   (cond
+    (nil? a) [b IS_NULL]
+    (nil? b) [a IS_NULL]
+    :else [a EQUALS b])))
+
+(defmethod render-fn '<>
+  [_ a b]
+  (par
+   (cond
+    (nil? a) [b IS_NOT_NULL]
+    (nil? b) [a IS_NOT_NULL]
+    :else [a NOT_EQUALS b])))
+
+(defmethod render-fn '+
+  ([_ x] (par [UPLUS x]))
+  ([_ x & r] (par (interpose PLUS (cons x r)))))
+
+(defmethod render-fn '-
+  ([_ x] (par [UMINUS x]))
+  ([_ a & r] (par (interpose MINUS (cons a r)))))
+
+(defmethod render-fn '*
+  [_ x & r] (par (interpose MULTIPLY (cons x r))))
+
+(defmethod render-fn '/
+  [_ x y] (par [x DIVIDE y]))
+
+(defmethod render-fn 'not
+  [_ x]
+  (par NOT x))
+
+(defmethod render-fn '<
+  [_ a b]
+  (par a LESS b))
+
+(defmethod render-fn '>
+  [_ a b]
+  (par a GREATER b))
+
+(defmethod render-fn '<=
+  [_ a b]
+  (par a LESS_EQUAL b))
+
+(defmethod render-fn '>=
+  [_ a b]
+  (par a GREATER_EQUAL b))
+
+(defmethod render-fn 'nil?
+  [_ x]
+  (par x IS_NULL))
+
+(defmethod render-fn 'not-nil?
+  [_ x]
+  (par x IS_NOT_NULL))
+
+(defmethod render-fn 'not-in?
+  [_ a b]
+  (cond
+   (empty? b) const-true
+   (map? b) (par a NOT_IN (parenthesis b))
+   :else (par a NOT_IN (parenthesis (comma-list b)))))
+
+(defmethod render-fn 'in?
+  [_ a b]
+  (cond
+   (empty? b) const-false
+   (map? b) (par a IN (parenthesis b))
+   :else (par a IN (parenthesis (comma-list b)))))
+
+(defmethod render-fn 'count
+  ([_ r] [COUNT (parenthesis r)])
+  ([_ d r]
+     (case d
+       :distinct [COUNT (par DISTINCT r)]
+       nil [COUNT (par r)]
+       (illegal-argument "Unknown modifier " d))))
+
+(defmethod render-fn 'max
+  [_ x]
+  [MAX (par x)])
+
+(defmethod render-fn 'min
+  [_ x]
+  [MIN (par x)])
+
+(defmethod render-fn 'avg
+  [_ x]
+  [AVG (par x)])
+
+(defmethod render-fn 'sum
+  [_ x]
+  [SUM (par x)])
+
+(defmethod render-fn 'exists?
+  [_ q]
+  (par EXISTS (par q)))
+
+(defmethod render-fn 'not-exists?
+  [_ q]
+  (par NOT_EXISTS (par q)))
+
+(defmethod render-fn 'some
+  ([_ q] [SOME (par q)])
+  ([_ f q] [SOME (par (attach-field f q))]))
+
+(defmethod render-fn 'any
+  ([_ q] [ANY (par q)])
+  ([_ f q] [ANY (par (attach-field f q))]))
+
+(defmethod render-fn 'all
+  ([_ q] [ALL (par q)])
+  ([_ f q] [ALL (par (attach-field f q))]))
+
+(defmethod render-fn 'like?
+  [_ a b]
+  [a LIKE b ESCAPE like-pattern-escaping-sql])
+
+(defmethod render-fn 'begins?
+  [_ a b]
+  (check-argument (string? b) "Pattern should be string.")
+  [a LIKE (str (escape-like-pattern b) "%")
+   ESCAPE like-pattern-escaping-sql])
