0dfc6c9a274fae2a0a264f5b8f0ef3d78570416c
Andrei Zhlobich <a_zhlobich@wargaming.net>
2014-10-29 11:18:44 +0300

    Upgrade libs

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.jdbc "0.3.4"]]
+                 [org.clojure/java.jdbc "0.3.6"]]
   :profiles
   {:h2
-   {:dependencies [[com.h2database/h2 "1.4.180"]]
+   {:dependencies [[com.h2database/h2 "1.4.182"]]
     :test-paths ["test-profile/h2"]},
    :mysql
-   {:dependencies [[mysql/mysql-connector-java "5.1.31"]]
+   {:dependencies [[mysql/mysql-connector-java "5.1.33"]]
     :test-paths ["test-profile/mysql"]},
    :pgsql
    {:dependencies [[org.postgresql/postgresql "9.3-1102-jdbc41"]]
